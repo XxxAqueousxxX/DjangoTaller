699a1b63a635a8c7f8e0140634b47dd664bc5b1f
XxxAqueousxxX <trujillorojas88@gmail.com>
2022-07-07 23:33:41 -0500

    redygo

--- DjangoTaller/settings.py
+++ DjangoTaller/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fvrk(or=#g4sgx_1^957%se!_n_ra3mjd37e4coiorgvosooc9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 # configuracion de cors
